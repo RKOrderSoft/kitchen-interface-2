abb6b9920f6531d088204863a6e4da8ec50483f9
HenryDeng9135 <HenryDeng9135@gmail.com>
2018-09-14 09:26:38 +1000

    last commit

--- js/main.js
+++ js/main.js
@@ -1,60 +1,75 @@
 ﻿var kitchen = new orderSoftClient();  
 
 window.onload = async () => { 
-
     document.getElementById("modalAuthenticate").classList.add("shown");
     document.getElementById("orderHistory").classList.add('hide');
-    //check server ip
+    //authenticate server ip
     document.getElementById("btnNext").onclick = async () => {
         try {
             var ip = "http://" + document.getElementById("ip").value + "/" ; 
             await kitchen.init(ip);
             document.getElementById("serverAuthenticate").classList.add("hide");
             document.getElementById("userLogin").classList.remove("hide");
+            document.getElementById("userLogin").classList.add("shown")
         } catch (error) {
             document.getElementById("wrongIP").innerHTML = error.toString();
         }
     }
-
-    //user login  
     document.getElementById("btnLogin").onclick = async () => {
         try {
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
+            var username = document.getElementById("username").value; //enter username
+            var password = document.getElementById("password").value; //enter password
             await kitchen.authenticate(username, password);
 
             document.getElementById("modalAuthenticate").classList.remove("shown");
             getOrderItems();
-            setInterval(getCompletedOrder, 3000);
-            setInterval(getOrderItems, 3000);
-            window.setTimeout(getOrderItems, 500);
+            setInterval(getUnpaidOrders, 3000); //timer to refresh history of orders
+            setInterval(getOrderItems, 3000);     // timer to refresh geting orders
+            window.setTimeout(getOrderItems, 500);//set system time
         } catch (error) {
             document.getElementById("wrongLogin").innerHTML = "Wrong username/password"
         }
+
+    }
+    //open authenticate help window
+    document.getElementById("btnhelpAuthenticate").onclick = () => {
+        document.getElementById("helpAuthenticate").classList.add("shown");
+    };
+    //close authenticate help window
+    document.getElementById("btncloseHelpAuthenticate").onclick = () => {
+        document.getElementById("helpAuthenticate").classList.remove("shown");
+    };
+    //user login  
+    
+    //return to server input page
+    document.getElementById("btnReturn").onclick = () => {
+        document.getElementById("serverAuthenticate").classList.add("shown");
+        document.getElementById("serverAuthenticate").classList.remove("hide");
+        document.getElementById("userLogin").classList.remove("shown");
+        document.getElementById("userLogin").classList.add("hide");
         
     }
-
     //refresh page
     document.getElementById("refresh").onclick = () => {
         getOrderItems();
     }
 
-    //open history modal
-    document.getElementById("viewHistory").onclick = () => {
-        document.getElementById("orderHistory").classList.remove('hide');
-        document.getElementById("orderHistory").classList.add("shown");
-        console.log('viewHistory');
+    
+    //ktichen help
+    document.getElementById("btnmenuHelp").onclick = () => {
+        document.getElementById("kitchenHelp").classList.add("shown");
     }
-
+    document.getElementById('closeKitchenHelp').onclick = () => {
+        document.getElementById('kitchenHelp').classList.remove("shown");
+    }
     // get system time
     setInterval(timerTick, 500);
 }
 
 
 //get unpaid orders
-async function getCompletedOrder() {
+async function getUnpaidOrders() {
     var completeOrderIds = (await kitchen.unpaidOrders()).unpaidOrders; //get unpaid orders
-
     for (var i = 0; i < completeOrderIds.length; i++) { //repeat for each unpaid order
         var orderToGet = completeOrderIds[i];
         var completeOrders = (await kitchen.getOrder("orderId", orderToGet)).order; //get orderIds
@@ -63,34 +78,37 @@ async function getCompletedOrder() {
 }
 
 async function displayCompletedOrders(orderToDisplay) {
+    /*
     var newOrder = document.createElement("tr");
 
     //separate dishIds in dishes
     var completeDishesArray = (orderToDisplay.dishes.split(','));
-    //console.log(completeDishesArray)
+
+
     //get list of dishes in order
     for (var i = 0; i < completeDishesArray.length; i++) { //get dish name from Id
-        var dishId = dishesArray[i];
-        console.log(dishId);
+        var dishId = completeDishesArray[i];
         var dishName = await getDishName(dishId);
-        var newDish = document.createElement("li");
-        newDish.appendChild(document.createTextNode(dishName.dishName));
+        var liDishes = document.createElement("li");
+        //document.createTextNode(liDishes.dishName);
+        newDish.appendChild(
         //get size 
         if (dishName.sizeName !== undefined) {
-            newDish.appendChild(document.createTextNode(dishName.sizeName)); //display size
+            var sizeSpan = document.createElement("span");
+            sizeSpan.classList.add("sizeSpan");
+            sizeSpan.innerHTML = dishName.sizeName;
+            newDish.appendChild(sizeSpan); //display size
         };
         newDish.querySelector("completedOrderDishes").appendChild(newDish);
     }
     newOrder.appendChild(document.createElement('td').innerHTML = orderToDisplay.tableNumber);
     newOrder.appendChild(document.createElement('td').innerHTML = orderToDisplay.timecompleted);
     document.getElementById("completeOrders").appendChild(newOrder);
-    
+    */
 }
 //get open orders and display them
 async function getOrderItems() {
     var openOrderIds = (await kitchen.openOrders()).openOrders; // openOrderIds is an array of strings
-
-    // Get all orders
     var toDisplay = [];
     //get each order
     for (var i = 0; i < openOrderIds.length; i++) {
